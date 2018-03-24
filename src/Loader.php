c2b2c0429650d906bc5983c66c364c64291fadff
Jeremy Desvaux <jeremy.desvaux@wonderful.fr>
2018-03-24 15:54:55 +0100

    New minimalist framework init

---
+++ src/Loader.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace WonderWp\Component;
+
+
+use WonderWp\Component\DependencyInjection\Container;
+use WonderWp\Component\DependencyInjection\SingletonInterface;
+use WonderWp\Component\DependencyInjection\SingletonTrait;
+
+class Loader implements SingletonInterface
+{
+    use SingletonTrait {
+        SingletonTrait::buildInstance as createInstance;
+    }
+
+    /** @inheritdoc */
+    public static function buildInstance()
+    {
+        $instance = static::createInstance();
+
+        $instance->load();
+
+        return $instance;
+    }
+
+    /**
+     * init the framework, loads the config, the autoloader, error handling...
+     */
+    public function load()
+    {
+        // Create DI container
+        $container = Container::getInstance();
+
+        /**
+         * Define Paths
+         */
+        $container['path_root']                           = ABSPATH . '../../'; // root
+        $container['path_framework_root']                 = __DIR__; // Framework root
+        $container['wwp.path.defaultlanguagedir.plugins'] = trailingslashit(WP_LANG_DIR) . DIRECTORY_SEPARATOR . 'plugins' . DIRECTORY_SEPARATOR;
+
+        /**
+         * Define Services
+         */
+
+        // Autoloader
+        /**
+         * @param Container $container
+         *
+         * @return mixed
+         */
+        $container['wwp.autoLoader'] = function (Container $container) {
+            return require($container['path_root'] . 'vendor/autoload.php');
+        };
+
+        //FileSystem
+        $container['wwp.fileSystem'] = function () {
+            global $wp_filesystem;
+            if (empty($wp_filesystem)) {
+                require_once(ABSPATH . '/wp-admin/includes/file.php');
+                WP_Filesystem();
+            }
+
+            return $wp_filesystem;
+        };
+
+        do_action('wonderwp.loader.load');
+
+        /**
+         * Make container available
+         */
+        Container::setInstance($container);
+    }
+}
