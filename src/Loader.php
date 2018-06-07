c1a2dd7a728154f43718c0842f3db9e4638d52cc
Jeremy Desvaux <jeremy.desvaux@wonderful.fr>
2018-06-07 22:23:33 +0200

    dependencies and loader adjustments

--- src/Loader.php
+++ src/Loader.php
@@ -6,11 +6,17 @@ namespace WonderWp\Bundle;
 use WonderWp\Component\Asset\Asset;
 use WonderWp\Component\Asset\AssetManager;
 use WonderWp\Component\Asset\DirectAssetEnqueuer;
-use WonderWp\Component\Asset\JsonAssetEnqueuer;
 use WonderWp\Component\Asset\JsonAssetExporter;
+use WonderWp\Component\Cache\TransientCache;
 use WonderWp\Component\DependencyInjection\Container;
 use WonderWp\Component\DependencyInjection\SingletonInterface;
 use WonderWp\Component\DependencyInjection\SingletonTrait;
+use WonderWp\Component\Form\Form;
+use WonderWp\Component\Form\FormValidator;
+use WonderWp\Component\Form\FormView;
+use WonderWp\Component\Form\FormViewReadOnly;
+use WonderWp\Component\Hook\HookManager;
+use WonderWp\Component\Mailing\WpMailer;
 use WonderWp\Component\Routing\Router\Router;
 
 class Loader implements SingletonInterface
@@ -63,19 +69,28 @@ class Loader implements SingletonInterface
             return new Router();
         };
         //Assets
-        $container['wwp.assets.manager']       = function () {
+        $container['wwp.asset.manager']       = function () {
             return AssetManager::getInstance();
         };
-        $container['wwp.assets.exporterClass'] = JsonAssetExporter::class;
-        $container['wwp.assets.assetClass']    = Asset::class;
-        $container['wwp.assets.manifest.path'] = $container['path_root'] . '/assets.json';
-        $container['wwp.assets.enqueuer']      = function ($container) {
+        $container['wwp.asset.exporterClass'] = JsonAssetExporter::class;
+        $container['wwp.asset.assetClass']    = Asset::class;
+        $container['wwp.asset.manifest.path'] = $container['path_root'] . '/assets.json';
+        $container['wwp.asset.enqueuer']      = function ($container) {
             return new DirectAssetEnqueuer();
         };
-        $container['wwp.assets.folder.prefix'] = './';
-        $container['wwp.assets.folder.dest']   = '';
-        $container['wwp.assets.folder.path']   = str_replace(trim(get_bloginfo('url'),'/'), '', str_replace(trim(network_site_url(),'/'), '', get_stylesheet_directory_uri()));
+        $container['wwp.asset.folder.prefix'] = './';
+        $container['wwp.asset.folder.dest']   = '';
+        $container['wwp.asset.folder.path']   = str_replace(trim(get_bloginfo('url'),'/'), '', str_replace(trim(network_site_url(),'/'), '', get_stylesheet_directory_uri()));
 
+        //Emails
+        $container['wwp.mailing.mailer'] = $container->factory(function () {
+            return new WpMailer();
+        });
+
+        //Cache
+        $container['wwp.cache.cache'] = function () {
+            return new TransientCache();
+        };
 
         //FileSystem
         $container['wwp.fileSystem'] = function () {
@@ -87,6 +102,25 @@ class Loader implements SingletonInterface
 
             return $wp_filesystem;
         };
+
+        //Hook Manager
+        $container['wwp.hook.manager'] = function(){
+            return new HookManager();
+        };
+
+        //Forms
+        $container['wwp.form.form']              = $container->factory(function () {
+            return new Form();
+        });
+        $container['wwp.form.view']          = $container->factory(function () {
+            return new FormView();
+        });
+        $container['wwp.form.view.readOnly'] = $container->factory(function () {
+            return new FormViewReadOnly();
+        });
+        $container['wwp.form.validator']     = $container->factory(function () {
+            return new FormValidator();
+        });
 
         do_action('wonderwp.loader.load');
 
